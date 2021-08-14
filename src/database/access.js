5e975414adfb2bcbd96c58e549f5dbc45af97e15
Promise <me@promise.solutions>
2021-08-14 19:15:15 +0200

    Remove unused code (plan was to add oauth to get guilds, got too complicated and non-user friendly)

--- src/database/access.js
+++ src/database/access.js
@@ -3,10 +3,6 @@ const mongoose = require("mongoose");
 const accessSchema = new mongoose.Schema({
   user: { type: String, required: true },
   servers: { type: [String], default: [] },
-  tokens: { type: {
-    access: { type: String, required: true },
-    refresh: { type: String, required: true }
-  }, default: {} },
   expires: { type: Date, required: true },
 });
 
