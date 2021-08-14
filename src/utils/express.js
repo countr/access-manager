8ab39928ab9987b47828c205924c0f288631d4ce
Promise <me@promise.solutions>
2021-08-14 19:33:15 +0200

    Allow webserver to be disabled

--- src/utils/express.js
+++ src/utils/express.js
@@ -5,6 +5,6 @@ app.use(express.json());
 
 app.get("/ping", (_, response) => response.sendStatus(200));
 
-app.listen(config.port);
+if (config.port) app.listen(config.port);
 
 module.exports = app;
