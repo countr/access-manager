bb017986a951afa1ed0062688db8f4579a1c4765
Glenn <glenn@not-tech.support>
2022-07-05 17:02:58 +0200

    Add createAccess function

--- src/database/access.ts
+++ src/database/access.ts
@@ -1,13 +1,18 @@
 import { Access } from "./models/Access";
 import type { AccessDocument } from "./models/Access";
 import type { Snowflake } from "discord.js";
+import config from "../config";
 
 export async function getAccess(userId: Snowflake): Promise<AccessDocument | null> {
   return Access.findOne({ userId });
 }
 
+export function createAccess(userId: Snowflake): AccessDocument {
+  return new Access({ userId, expires: new Date(Date.now() + config.access.expireGrace) });
+}
+
 export async function getOrCreateAccess(userId: Snowflake): Promise<AccessDocument> {
-  return await getAccess(userId) ?? new Access({ userId });
+  return await getAccess(userId) ?? createAccess(userId);
 }
 
 export async function addServer(userId: Snowflake, guildId: Snowflake): Promise<boolean> {
