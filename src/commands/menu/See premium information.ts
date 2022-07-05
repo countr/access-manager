4c457a58676f7534f86a415b7af441b6b5f3291f
Glenn <glenn@not-tech.support>
2022-07-05 17:03:15 +0200

    Refactor menu command

--- src/commands/menu/See premium information.ts
+++ src/commands/menu/See premium information.ts
@@ -1,16 +1,17 @@
+import { createAccess, getAccess } from "../../database";
 import { ApplicationCommandType } from "discord.js";
 import type { ContextMenuCommand } from ".";
 import config from "../../config";
-import { getAccess } from "../../database";
 import { getTokens } from "../../constants/tokens";
 
 const command: ContextMenuCommand = {
   type: ApplicationCommandType.User,
   async execute(interaction, target, confidentialAccess) {
-    const access = await getAccess(target.id);
-    if (!access) return void interaction.reply({ content: "❌ This user does not have access to Countr Premium.", ephemeral: true });
-
     const tokens = getTokens(target);
+
+    let access = await getAccess(target.id);
+    if (!access && tokens) access = createAccess(target.id);
+    if (!access) return void interaction.reply({ content: "❌ This user does not have access to Countr Premium.", ephemeral: true });
 
     return void interaction.reply({
       embeds: [
@@ -22,7 +23,7 @@ const command: ContextMenuCommand = {
           },
           color: config.color,
           fields: [
-            { name: "Servers", value: `**${access.guildIds.length}/${tokens} servers with Premium:** ${confidentialAccess ? `\n${access.guildIds.map(guildId => `• \`${guildId}\``).join("\n")}` : "*(confidential)*"}`, inline: true },
+            { name: "Servers", value: `**${access.guildIds.length}/${tokens} servers with Premium:** ${confidentialAccess ? `\n${access.guildIds.map(guildId => `• \`${guildId}\``).join("\n") || "*(no servers in list)*"}` : "*(confidential)*"}`, inline: true },
             { name: "Scheduled expiration", value: tokens ? "*Not scheduled (user still has access)*" : `<t:${Math.floor(access.expires.getTime() / 1000)}:R>`, inline: true },
           ],
         },
